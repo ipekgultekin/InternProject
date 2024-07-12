e3c369b1885e0cf4f17daf560f19fac84ba0c4ab
İpek Gültekin <ipek.gultekiin.01@gmail.com>
2024-07-12 16:49:54 +0300

    güncel

--- StokTakipStajyer2/StokTakipStajyer2/Controllers/HomeController.cs
+++ StokTakipStajyer2/StokTakipStajyer2/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,7 +9,6 @@ using StokTakipStajyer2.Models;
 
 namespace StokTakipStajyer2.Controllers
 {
-   
     public class HomeController : Controller
     {
         StokTakipDBEntities stokdata = new StokTakipDBEntities();
@@ -17,46 +17,43 @@ namespace StokTakipStajyer2.Controllers
         {
             return View();
         }
+
         public ActionResult Admin()
         {
             return View();
         }
+
         [HttpGet]
         public ActionResult Giris()
         {
             return View();
-
         }
-
 
         [HttpPost]
         public ActionResult Giris(KULLANICI fc)
         {
-            var info = stokdata.KULLANICI.FirstOrDefault(x=> x.KUL_USERNAME == fc.KUL_USERNAME && x.KUL_SIFRE == fc.KUL_SIFRE);
-            
-            if(info != null)
+            var info = stokdata.KULLANICI.FirstOrDefault(x => x.KUL_USERNAME == fc.KUL_USERNAME && x.KUL_SIFRE == fc.KUL_SIFRE);
+
+            if (info != null)
             {
                 FormsAuthentication.SetAuthCookie(info.KUL_USERNAME, false);
                 Session["ID"] = info.KUL_ID.ToString();
+                Session["KullaniciTipi"] = info.KUL_TIP;
                 ViewBag.ErrorMessage = "Kullanıcı girişi başarılı.";
 
                 return RedirectToAction("KullaniciEkle");
-
             }
-
             else
             {
                 ViewBag.ErrorMessage = "Geçersiz kullanıcı adı veya şifre.";
                 return View();
             }
-     
-
         }
 
         [HttpGet]
         public ActionResult KullaniciEkle()
         {
-            if (Session["ID"] == null)
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
             {
                 return RedirectToAction("Giris");
             }
@@ -68,7 +65,7 @@ namespace StokTakipStajyer2.Controllers
         [HttpPost]
         public ActionResult KullaniciEkle(KULLANICI kullanici)
         {
-            if (Session["ID"] == null)
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
             {
                 return RedirectToAction("Giris");
             }
@@ -83,25 +80,21 @@ namespace StokTakipStajyer2.Controllers
                     KUL_SIFRE = kullanici.KUL_SIFRE,
                     KUL_TIP = kullanici.KUL_TIP,
                     STATU = kullanici.STATU,
-
                     OLUSTURAN_KULLANICI = Convert.ToInt32(Session["ID"]),
                     OLUSTURMA_TARIHI = DateTime.Now,
                     GUNCELLEME_TARIHI = DateTime.Now,
                     GUNCELLEYEN_KULLANICI = Convert.ToInt32(Session["ID"]),
-
                 };
 
                 stokdata.KULLANICI.Add(model);
                 stokdata.SaveChanges();
-                ViewBag.Message = "Kullanıcı başarıyla eklendi.";
+                TempData["SuccessMessage"] = "Kullanıcı başarıyla eklendi.";
 
-                return RedirectToAction("Index");
+                return RedirectToAction("KullaniciListele");
             }
 
             return View(kullanici);
         }
-
-
 
         [HttpGet]
         public ActionResult KullaniciListele(string searchString = null)
@@ -129,7 +122,27 @@ namespace StokTakipStajyer2.Controllers
         [HttpGet]
         public ActionResult KullaniciSil(int id)
         {
-            if (Session["ID"] == null)
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var kullanici = stokdata.KULLANICI.Find(id);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+
+            stokdata.KULLANICI.Remove(kullanici);
+            stokdata.SaveChanges();
+            TempData["SuccessMessage"] = "Kullanıcı başarıyla silindi.";
+            return RedirectToAction("KullaniciListele");
+        }
+
+        [HttpGet]
+        public ActionResult KullaniciGuncelle(int id)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
             {
                 return RedirectToAction("Giris");
             }
@@ -143,24 +156,294 @@ namespace StokTakipStajyer2.Controllers
             return View(kullanici);
         }
 
-        [HttpPost, ActionName("KullaniciSil")]
-        public ActionResult KullaniciSilConfirmed(int id)
+        [HttpPost]
+        public ActionResult KullaniciGuncelle(KULLANICI kullanici)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+            if (ModelState.IsValid)
+            {
+                var updateKullanici = stokdata.KULLANICI.Find(kullanici.KUL_ID);
+
+                if (updateKullanici != null)
+                {
+                    updateKullanici.KUL_USERNAME = kullanici.KUL_USERNAME;
+                    updateKullanici.KUL_AD = kullanici.KUL_AD;
+                    updateKullanici.KUL_SOYAD = kullanici.KUL_SOYAD;
+                    updateKullanici.KUL_SIFRE = kullanici.KUL_SIFRE;
+                    updateKullanici.KUL_TIP = kullanici.KUL_TIP;
+                    updateKullanici.STATU = kullanici.STATU;
+
+                    stokdata.SaveChanges();
+                    TempData["SuccessMessage"] = "Kullanıcı başarıyla güncellendi.";
+                    return RedirectToAction("KullaniciListele");
+                }
+            }
+
+            return View(kullanici);
+        }
+
+
+        [HttpGet]
+        public ActionResult DepoEkle()
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+            var depoModel = new DEPO();
+
+            return View(depoModel);
+        }
+
+        [HttpPost]
+        public ActionResult DepoEkle(DEPO depo)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var depoModel = new DEPO()
+                {
+                  
+                    DEPO_ADI = depo.DEPO_ADI,
+                    STATU = depo.STATU,
+                    OLUSTURAN_KULLANICI = Convert.ToInt32(Session["ID"]),
+                    OLUSTURMA_TARIHI = DateTime.Now,
+                    GUNCELLEME_TARIHI = DateTime.Now,
+                    GUNCELLEYEN_KULLANICI = Convert.ToInt32(Session["ID"]),
+                };
+
+                stokdata.DEPO.Add(depoModel);
+                stokdata.SaveChanges();
+                TempData["SuccessMessage"] = "Depo başarıyla eklendi.";
+
+                return RedirectToAction("DepoListele");
+            }
+
+            return View(depo);
+        }
+
+
+        [HttpGet]
+        public ActionResult DepoListele(string searchString = null)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var depolar = from d in stokdata.DEPO select d;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                depolar = depolar.Where(s => s.DEPO_ADI.Contains(searchString) ||
+                                             s.STATU.ToString().Contains(searchString));
+            }
+
+            ViewBag.SuccessMessage = TempData["SuccessMessage"];
+            return View(depolar.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult DepoSil(int id)
         {
             if (Session["ID"] == null)
             {
                 return RedirectToAction("Giris");
             }
 
-            var kullanici = stokdata.KULLANICI.Find(id);
-            if (kullanici != null)
+            var depo = stokdata.DEPO.Find(id);
+            if (depo != null)
             {
-                stokdata.KULLANICI.Remove(kullanici);
+                stokdata.DEPO.Remove(depo);
                 stokdata.SaveChanges();
-                ViewBag.Message = "Kullanıcı başarıyla silindi.";
+                TempData["SuccessMessage"] = "Depo başarıyla silindi.";
             }
 
-            return RedirectToAction("KullaniciListele");
+            return RedirectToAction("DepoListele");
         }
+
+        [HttpGet]
+        public ActionResult DepoGuncelle(int id)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var depo = stokdata.DEPO.Find(id);
+            if (depo == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(depo);
+        }
+
+        [HttpPost]
+        public ActionResult DepoGuncelle(DEPO depo)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+            if (ModelState.IsValid)
+            {
+                var updateDepo = stokdata.DEPO.Find(depo.DEPO_ID);
+
+                if (updateDepo != null)
+                {
+                    updateDepo.DEPO_ADI = depo.DEPO_ADI;
+                    updateDepo.STATU = depo.STATU;
+
+                    stokdata.SaveChanges();
+                    TempData["SuccessMessage"] = "Depo başarıyla güncellendi.";
+                    return RedirectToAction("DepoListele");
+                }
+            }
+
+            return View(depo);
+        }
+
+
+        [HttpGet]
+        public ActionResult AltDepoEkle()
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+            var altdepoModel = new ALT_DEPO();
+
+            return View(altdepoModel);
+        }
+
+        [HttpPost]
+        public ActionResult AltDepoEkle(ALT_DEPO depo)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var depoModel = new ALT_DEPO()
+                {
+                    ALT_DEPO_ADI = depo.ALT_DEPO_ADI,
+                    STATU = depo.STATU,
+                    OLUSTURAN_KULLANICI = Convert.ToInt32(Session["ID"]),
+                    OLUSTURMA_TARIHI = DateTime.Now,
+                    GUNCELLEME_TARIHI = DateTime.Now,
+                    GUNCELLEYEN_KULLANICI = Convert.ToInt32(Session["ID"]),
+                };
+
+                stokdata.ALT_DEPO.Add(depoModel);
+                stokdata.SaveChanges();
+                TempData["SuccessMessage"] = "Alt depo başarıyla eklendi.";
+
+                return RedirectToAction("AltDepoListele");
+            }
+
+            return View(depo);
+        }
+
+
+        [HttpGet]
+        public ActionResult AltDepoListele(string searchString = null)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var depolar = from d in stokdata.ALT_DEPO select d;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                depolar = depolar.Where(s => s.ALT_DEPO_ADI.Contains(searchString) ||
+                                             s.STATU.ToString().Contains(searchString));
+            }
+
+            ViewBag.SuccessMessage = TempData["SuccessMessage"];
+            return View(depolar.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AltDepoSil(int id)
+        {
+            if (Session["ID"] == null)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var depo = stokdata.ALT_DEPO.Find(id);
+            if (depo != null)
+            {
+                stokdata.ALT_DEPO.Remove(depo);
+                stokdata.SaveChanges();
+                TempData["SuccessMessage"] = "Depo başarıyla silindi.";
+            }
+
+            return RedirectToAction("AltDepoListele");
+        }
+
+        [HttpGet]
+        public ActionResult AltDepoGuncelle(int id)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+
+            var depo = stokdata.ALT_DEPO.Find(id);
+            if (depo == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(depo);
+        }
+
+        [HttpPost]
+        public ActionResult AltDepoGuncelle(ALT_DEPO depo)
+        {
+            if (Session["ID"] == null || (int)Session["KullaniciTipi"] != 1)
+            {
+                return RedirectToAction("Giris");
+            }
+            if (ModelState.IsValid)
+            {
+                var updateDepo = stokdata.ALT_DEPO.Find(depo.ALT_DEPO_ID);
+
+                if (updateDepo != null)
+                {
+                    updateDepo.ALT_DEPO_ADI = depo.ALT_DEPO_ADI;
+                    updateDepo.STATU = depo.STATU;
+
+                    stokdata.SaveChanges();
+                    TempData["SuccessMessage"] = "Depo başarıyla güncellendi.";
+                    return RedirectToAction("AltDepoListele");
+                }
+            }
+
+            return View(depo);
+        }
+
+
+        public ActionResult Cikis()
+        {
+            FormsAuthentication.SignOut();
+            Session.Clear();
+            return RedirectToAction("Giris");
+        }
+
 
         public ActionResult About()
         {
